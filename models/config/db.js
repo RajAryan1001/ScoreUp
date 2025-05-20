d4e554ff4d15cfb38f4d7e31cfb2c3dd50699a48
rajaryan7803 <rajaryan7803@gmail.com>
2025-05-20 12:19:12 +0530

    Completed

--- models/config/db.js
+++ models/config/db.js
@@ -1,20 +1,20 @@
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://Raj:Rajaryan@cluster0.crfzpz6.mongodb.net/Education').then(()=>{
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Raj:Rajaryan@cluster0.crfzpz6.mongodb.net/Education').then(()=>{
     
-//     console.log('connected');
-// }).catch((err)=>{
-//     console.log(err);
-// });
+    console.log('connected');
+}).catch((err)=>{
+    console.log(err);
+});
 
 // // test
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Education', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
+// mongoose.connect('mongodb://localhost:27017/Education', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((err) => {
+//   console.error('MongoDB connection error:', err);
+// });
